7cdfd597c88df76c6c20bf6c2ace8ec39017eca0
Alvaro Huanca <alva0741@gmail.com>
2026-03-18 23:03:41 -0400

    TH-34: Initial configuration for the project

---
+++ src/test/java/com/aihc/transithub/TransitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aihc.transithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
